4c97f3614135f3efc5228c8748dabe8ff5b39fee
roie123 <roieivri123@gmail.com>
2023-03-06 21:58:29 +0200

    Enabled Scheduling

--- src/main/java/com/RoieIvri/CouponsPhase2/CouponsPhase2Application.java
+++ src/main/java/com/RoieIvri/CouponsPhase2/CouponsPhase2Application.java
@@ -7,8 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CouponsPhase2Application {
 
 
