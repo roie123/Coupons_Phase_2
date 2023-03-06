b5a9210285d091f589bc9048827c565fefdd725f
roie123 <roieivri123@gmail.com>
2023-03-06 17:07:20 +0200

    Added the Login feature and the purchaseCoupon feature

--- src/main/java/com/RoieIvri/CouponsPhase2/CUSTOMER/CustomerService.java
+++ src/main/java/com/RoieIvri/CouponsPhase2/CUSTOMER/CustomerService.java
@@ -1,11 +1,16 @@
 package com.RoieIvri.CouponsPhase2.CUSTOMER;
 
 import com.RoieIvri.CouponsPhase2.COMPANY.Company;
+import com.RoieIvri.CouponsPhase2.COUPON.Coupon;
+import com.RoieIvri.CouponsPhase2.COUPON.CouponException;
+import com.RoieIvri.CouponsPhase2.COUPON.CouponService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
@@ -15,8 +20,15 @@ public class CustomerService {
     @Autowired
     private CustomerRepo customerRepo;
 
-    public boolean isObjectExist(String email, String password) throws Exception {
-        return false;
+    private final CouponService couponService;
+
+    public Customer login(String email, String password) throws Exception {
+        if (customerRepo.existsByEmailAndPassword(email,password)){
+            Customer customer = customerRepo.getCustomerByEmailAndPassword(email,password);
+            System.out.println("LOGGED IN CUSTOMER ===> "+customer);
+            return customer;
+        }
+        throw new CustomerException("CUSTOMER LOGIN VALUES ARE INCORRECT ");
     }
 
     public Customer addObject(Customer customer) throws Exception {
@@ -66,5 +78,31 @@ public class CustomerService {
         });
 
         updateObject(customer, customerId);
+    }
+
+@Transactional
+    public void purchaseCoupon(Long couponId,Long customerId) throws Exception {
+        Customer customer ;
+        customer = customerRepo.existsById(couponId) ? customerRepo.findById(customerId).get() : null;
+
+    for (Coupon c :
+            customer.getCoupons()) {
+        if (c.getId().intValue()==couponId.intValue()){
+            throw new CustomerException("CANT PURCHASE AN EXISTING COUPON");
+        }
+
+    }
+
+        if (customer!=null && customer.getCoupons().size()> -1 && couponService.existById(couponId) ){
+            Coupon coupon = couponService.getOneObject(couponId);
+            LocalDate localDate = LocalDate.now();
+            if (coupon.getEndDate().isBefore(localDate)){
+                throw new CustomerException("CANT PURCHASE AN OUT OF DATE COUPON ");
+            }
+            customer.getCoupons().add(coupon);
+            updateObject(customer,customerId);
+            return;
+        }
+        throw new CustomerException("CUSTOMER OR COUPON  NOT FOUND BY ID ");
     }
 }
