d03ee71e4f92a74e7c781553650c84cf21991cfa
roie123 <roieivri123@gmail.com>
2023-03-09 19:12:06 +0200

    Added test Calls

--- src/main/java/com/RoieIvri/CouponsPhase2/CouponsPhase2Application.java
+++ src/main/java/com/RoieIvri/CouponsPhase2/CouponsPhase2Application.java
@@ -2,6 +2,7 @@ package com.RoieIvri.CouponsPhase2;
 
 import com.RoieIvri.CouponsPhase2.TESTERS.AdminTester;
 import com.RoieIvri.CouponsPhase2.TESTERS.CompanyTester;
+import com.RoieIvri.CouponsPhase2.TESTERS.CustomerTester;
 import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -17,13 +18,15 @@ public class CouponsPhase2Application {
 
 	public static void main(String[] args) throws Exception {
 		ApplicationContext ctx = SpringApplication.run(CouponsPhase2Application.class, args);
-//		AdminTester adminTester1  = ctx.getBean(AdminTester.class);
-//		adminTester1.test();
+		AdminTester adminTester1  = ctx.getBean(AdminTester.class);
+		adminTester1.test();
 
 
 		CompanyTester companyTester =ctx.getBean(CompanyTester.class);
 		companyTester.test();
 
+		CustomerTester customerTester = ctx.getBean(CustomerTester.class);
+		customerTester.test();
 	}
 
 }
